534b292917ab81431235962a5dd0abbc360f2ea2
GabinTB <gabin48200@gmail.com>
2025-07-03 15:20:36 +0200

    Correct LM Test

--- src/realized_library/jump_detection/lm_test.py
+++ src/realized_library/jump_detection/lm_test.py
@@ -8,11 +8,13 @@ from realized_library.estimators.bipower_variation import compute as bpv
 from realized_library.estimators.multipower_variation import compute as mpv
 from realized_library._utils.std_norm_dist_moments import mu_x
 
+# # TODO: handling resampling
 def compute(
     prices: np.ndarray,
-    timestamps: np.ndarray,
+    # timestamps: np.ndarray,
     K: Optional[int] = 270,
-    resampling_timeframe: Optional[str] = "5m",
+    past_day_prices: Optional[np.ndarray] = None, # When K is large, then past_day_prices could be provided to fill missing data
+    # resampling_timeframe: Optional[str] = "5m",
     trading_day: int = 252,
     alpha: float = 0.01
 ) -> np.ndarray:
@@ -22,32 +24,42 @@ def compute(
         By Lee, S.S., and Mykland P.A.
         DOI: 10.1093/rfs/hhm056
     """
-    if prices.shape != timestamps.shape:
-        raise ValueError("Prices and timestamps must have the same shape.")
-    if len(timestamps) < 2:
-        raise ValueError("Timestamps must contain at least two entries.")
-    if np.diff(timestamps, n=2).any():
-        raise ValueError("Timestamps must be equally spaced. Please resample the data before applying the test.")
+    # if prices.shape != timestamps.shape:
+    #     raise ValueError("Prices and timestamps must have the same shape.")
+    # if len(timestamps) < 2:
+    #     raise ValueError("Timestamps must contain at least two entries.")
+    # if np.diff(timestamps, n=2).any():
+    #     raise ValueError("Timestamps must be equally spaced. Please resample the data before applying the test.")
     
-    if resampling_timeframe is not None:
-        prices, timestamps = resampling(prices=prices, timestamps=timestamps, sample_size=resampling_timeframe)
+    if past_day_prices is not None:
+        if len(past_day_prices) < K:
+            raise ValueError(f"If past_day_prices provided, it must contain at least {K} entries to be relevant, but got {len(past_day_prices)}.")
+        past_day_prices = past_day_prices[K:] # First K entries are not needed
+        final_prices = np.concatenate((past_day_prices, prices))
+    else:
+        final_prices = prices
+    
+    # if resampling_timeframe is not None:
+    #     prices, timestamps = resampling(prices=prices, timestamps=timestamps, sample_size=resampling_timeframe)
 
     n = len(prices)
-    dt = (trading_day * n)**(-1)
-    lb = int(dt**(-0.5))
-    hb = int(dt**(-1))
+    lb = np.sqrt(trading_day * n)
+    hb = trading_day * n
     if K is None:
-        K = int( (lb + hb) * 0.5 )
+        K = min( n // 2, int((lb + hb) * 0.5) )
     if not lb <= K <= hb:
-        warnings.warn(f"Lee and Mykland Test suggests dt^(-0.5) = {lb} < K < dt^(-1) = {hb} but you chose K = {K}.")
+        warnings.warn(f"Lee and Mykland Test suggests {lb} < K < {hb} but you chose K = {K}.")
 
-    subsamples = sliding_window_view(np.log(prices), window_shape=K)
-    Li = np.array([(sample[-1]/sample[-2]) / bpv(sample) for sample in subsamples])
+    if past_day_prices is not None:
+        subsamples = sliding_window_view(np.log(final_prices), window_shape=K)[K:] # We should have len(past_day_prices) == K
+    else:
+        subsamples = sliding_window_view(np.log(final_prices), window_shape=K)
+    
+    Li = np.array([(sample[-1]/sample[-2]) / bpv(sample) for sample in subsamples if len(sample) >= 2])
     # Li = np.array([(sample[-1]/sample[-2]) / mpv(sample, 2, 2) for sample in subsamples])
-
     c = mu_x(1)
     Sn = (c*(2*np.log(n))**0.5)**(-1)
-    Cn = (2 * np.log(n))**0.5 / c - (np.log(np.pi) + np.log(n)) * 0.5 * Sn
-    
+    Cn = (2 * np.log(n))**0.5 / c - 0.5 * (np.log(np.pi) + np.log(np.log(n))) * Sn
     threshold = -np.log(-np.log(1-alpha))
+    
     return (np.abs(Li) - Cn) * Sn**(-1) > threshold # Jump Flags: True if there is jump, False otherwise
